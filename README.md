d21149efd5f2acf0ac2126a0a61d84a4c5c00edd
Kevin Kobori <kevinyujikobori@gmail.com>
2024-06-03 19:22:24 -0300

    chore(config): add linters for code quality
    This commit integrates linters into the project configuration to enforce
    code quality standards and consistency. By adopting linting tools, this
    update aims to improve code readability, maintainability, and overall
    development practices, ensuring that the codebase adheres to best practices
    and avoids common pitfalls.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 Challenge: https://github.com/tractian/challenges/tree/main/mobile
 
 Figma: https://www.figma.com/design/IP50SSLkagXsUNWiZj0PjP/[Careers]-Flutter-Challenge-v2?node-id=0-1&t=rQqI5YMKR3wiZDHd-0
+
+1.
+```bash
+dart run build_runner watch --delete-conflicting-outputs
+```
--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TRACTIAN',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'TRACTIAN'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({required this.title, super.key});
 
   final String title;
 
